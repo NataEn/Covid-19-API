e7cdf504eb7797c8074f520af31eed286a0f2b76
natalie <natalie@enkin.co.il>
2021-01-07 20:59:16 +0200

    setting basic static files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
+
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 5000;
+const static = process.env.STATIC_PATH || path.join(__dirname, "../public");
+console.log(__dirname);
+
+app.use("/", express.static(static));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
