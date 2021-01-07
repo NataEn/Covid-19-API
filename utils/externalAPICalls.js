ffbd860e8f92eec731baa327d0e447204419afb8
natalie <natalie@enkin.co.il>
2021-01-07 23:55:22 +0200

    getting covid data from apis

--- utils/externalAPICalls.js
+++ utils/externalAPICalls.js
@@ -1,19 +1,36 @@
-const getCountries = () => {
-  const countries = fetch("https://restcountries.eu/rest/v2/all")
-    .then((response) => response.json())
-    .then((rawList) => {
-      const countries = rawList.map((item) => {
+const axios = require("axios");
+
+const getCountries = (continent) => {
+  const countries = axios
+    .get(`https://restcountries.herokuapp.com/api/v1/region/${continent}`)
+    .then((rawlist) => {
+      const countries = rawlist.data.map((item) => {
         return {
-          name: item.name,
-          code: item.alpha2Code,
-          flagUrl: `https://www.countryflags.io/${item.alpha2Code}/flat/32.png`,
+          name: item.name.common,
+          code: item.cca2,
+          flagUrl: `https://www.countryflags.io/${item.cca2}/flat/32.png`,
         };
       });
+      console.log(countries);
       return countries;
-    });
-
+    })
+    .catch((err) => console.error(err));
   return countries;
 };
+const getCountriesInfo = (country) => {
+  const countryInfo = axios
+    .get(`https://corona-api.com/countries`)
+    .then((rawlist) => {
+      const countryInfo = rawlist.data.data.filter(
+        (item) => item.name === country
+      );
+      console.log(countryInfo);
+      return countryInfo;
+    })
+    .catch((err) => console.error(err));
+  return countryInfo;
+};
+getCountriesInfo("Afghanistan");
 
 module.exports = {
   getCountries,
