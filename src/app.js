e790d513bf1dccff7efa7d45c008004d9c7a075f
natalie <natalie@enkin.co.il>
2021-01-07 22:49:36 +0200

    configuring express to serve static chartjs library to browser from nodemodules folder

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+//express config data
 const port = process.env.PORT || 5000;
 const static = process.env.STATIC_PATH || path.join(__dirname, "../public");
-console.log(__dirname);
+const partialsPath =
+  process.env.PARTIALS_PATH || path.join(__dirname, "../views/partials");
 
+//view engin and location setting
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
+
+//static files and assets
 app.use("/", express.static(static));
+app.use(
+  "/scripts",
+  express.static(path.join(__dirname, "../node_modules/chart.js/dist"))
+);
 
+//routing
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", { name: "1234" });
+});
+app.get("/api", (req, res) => {
+  res.send("Hello World! from api");
 });
 
 app.listen(port, () => {
